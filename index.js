0895200d293536d0f3298e9c5b35eeac39bff91f
jones <pnduru6@gmail.com>
2025-09-08 13:31:45 +0300

    done

---
+++ index.js
@@ -0,0 +1,24 @@
+function receivesAFunction(callback) {
+    console.log('Go to work');
+    callback();  
+}
+
+function receivesAFunction(callback) {
+    console.log('Go to work');
+    callback();
+  }
+
+  function returnsANamedFunction() {
+    return function namedFunction() {
+      console.log("I am a named function");
+    };
+  }
+  
+
+  function returnsAnAnonymousFunction() {
+    return function () {
+      console.log("I am an anonymous function");
+    };
+  }
+  
+ 
